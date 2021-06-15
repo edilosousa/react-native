5a619cc316c3d17aae5d00dadda6d192253cf92b
Edilo <edilo13@gmail.com>
2021-06-15 16:39:14 -0400

    pages filmes

--- pages/Filmes/index.js
+++ pages/Filmes/index.js
@@ -1,15 +1,35 @@
 import React,{Component} from 'react';
-import {SafeAreaView,Text,StyleSheet} from 'react-native';
+import {SafeAreaView,Text,StyleSheet,FlatList} from 'react-native';
 import styled from 'styled-components';
 import api from '../../config/configApi';
+import Filme from './item';
 
 export default class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            filmes: []
+        }
+    }
+
+    async componentDidMount(){
+        const response = await api.get('/usuarios');
+        this.setState({
+            filmes: response.data.usuarios
+        });
+        console.log(response.data.usuarios[0]['nome'])
+    }
+
     render(){
         return(
             <SafeAreaView style={styles.container}>
-                <Text>
-                    olá mundo.
-                </Text>
+                <FlatList
+                data={this.state.filmes}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => <Filme data={item} />}
+                >
+
+                </FlatList>
             </SafeAreaView>
         )
     }
